ecfe9148559377edc071d51bc8bf5e772a9e6434
PrajwalaNagekar <prajwalanagekar@gmail.com>
2025-07-30 00:32:41 +0530

    route added

--- src/routes/allRoutes/Admin/tools.router.js
+++ src/routes/allRoutes/Admin/tools.router.js
@@ -7,7 +7,7 @@ router.get('/all-tools', toolsController.allTools)
 router.put('/update/:id', toolsController.updateById)
 router.delete('/delete/:id', toolsController.deleteById)
 router.get('/get-by-id/:id', toolsController.getById)
-router.get('/get-engineer-assigned', toolsController.getEngineerByTool)
+router.get('/get-engineer-assigned/:id', toolsController.getEngineerByTool)
 
 
 //tools by employee
