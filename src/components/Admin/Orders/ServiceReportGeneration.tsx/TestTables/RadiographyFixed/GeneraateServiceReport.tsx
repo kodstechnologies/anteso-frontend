f66bff5f56c965b60c5505098ded3886ae3503db
PrajwalaNagekar <prajwalanagekar@gmail.com>
2026-08-04 09:50:00 +0530

    changes

--- src/components/Admin/Orders/ServiceReportGeneration.tsx/TestTables/RadiographyFixed/GeneraateServiceReport.tsx
+++ src/components/Admin/Orders/ServiceReportGeneration.tsx/TestTables/RadiographyFixed/GeneraateServiceReport.tsx
@@ -2165,8 +2165,8 @@ const RadiographyFixedContent: React.FC<RadiographyFixedProps> = ({ serviceId, q
           {[
             { label: "Nomenclature", name: "nomenclature", readOnly: true },
             { label: "Make", name: "make" },
-            { label: "Model", name: "model", readOnly: true },
-            { label: "Serial Number", name: "slNumber", readOnly: true },
+            { label: "Model", name: "model" },
+            { label: "Serial Number", name: "slNumber" },
             { label: "Category", name: "category" },
             { label: "Condition of Test Item", name: "condition" },
             { label: "Testing Procedure Number", name: "testingProcedureNumber" },
